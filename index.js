26b9279462ca568d8da94c2f262f905eac6e8948
MJaik <48388758+MJaik@users.noreply.github.com>
2019-04-03 20:07:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ client.on("ready", async () => {
   client.user.setActivity('&help | @MJaik#2003',{ type: 'PLAYING'});
 });
 
-client.on("message", async => {
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
   
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  
+  if(cmd === '${prefix}hello') {
+  return message.channel.send("Hello!");
+  }
 });
