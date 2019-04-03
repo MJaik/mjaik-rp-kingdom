becde1c295c3b2536ecefe8a135d027147f15466
MJaik <48388758+MJaik@users.noreply.github.com>
2019-04-03 15:09:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const Discord = require('discord.js')
+const client = new Discord.Client
+let prefix = '&'
 
+client.login(process.env.BOT_TOKEN)
+
+client.on("message", async message => {   
+client.user.setActivity('&help | @MJaik#2003',{ type: 'WATCHING' }) 
+if(message.author.bot) return;
+if(message.channel.type === "dm") return;
+  
+});
